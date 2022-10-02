e971aba1b422e2fe396aa3bcc480806041e9c87a
siqnax <siqnax@gmail.com>
2022-10-02 13:30:54 -0700

    task solution

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-"""Square Class
-This class don't do anything for the moment
-"""
+""" this module defines a empty class square """
 
 
 class Square:
+    """ empty class for task 0 """
     pass
